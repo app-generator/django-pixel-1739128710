e469e2ab81bc45c2c28755674c1324787a2a7a9a
redonesys <74498530+redonesys@users.noreply.github.com>
2025-02-20 17:28:39 +0100

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -10,7 +10,7 @@ from . import views
 urlpatterns = [
     # Pages
     path('', views.index),
-    path('about-us/', views.abouts_us, name='about_us'),
+    path('about-usss/', views.abouts_us, name='about_us'),
     path('contact-us/', views.contact_us, name='contact_us'),
     path('landing-freelancer/', views.landing_freelancer, name='landing_freelancer'),
     path('blank/', views.blank_page, name='blank'),
